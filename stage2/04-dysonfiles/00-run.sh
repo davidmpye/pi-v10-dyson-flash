f9360452560196c67eb8e1bb761009375d1d5442
David Pye <davidmpye@gmail.com>
2023-08-22 15:02:41 +0100

    Add missing tcl file and modify cfg to ensure it is installed properly

--- stage2/04-dysonfiles/00-run.sh
+++ stage2/04-dysonfiles/00-run.sh
@@ -5,6 +5,7 @@ install -m 755 files/openocd "${ROOTFS_DIR}/home/pi/"
 install -m 644 files/rpi1.cfg "${ROOTFS_DIR}/home/pi/"
 install -m 644 files/rpi2.cfg "${ROOTFS_DIR}/home/pi/"
 install -m 644 files/at91samdXX.cfg  "${ROOTFS_DIR}/home/pi/"
+install -m 644 files/swj-dp.tcl  "${ROOTFS_DIR}/home/pi/"
 
 install -m 644 files/V10_BMS.elf "${ROOTFS_DIR}/boot/"
 
