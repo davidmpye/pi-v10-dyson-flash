9643c3309c584cf94abdc29b8ed88c9ab0d2402f
David Pye <davidmpye@gmail.com>
2023-08-25 12:48:12 +0100

    Updates to script to make 5 attempts at unlock and flashing. Updates to openocd pi config files to reduce DEPRECATED warnings

--- stage2/04-dysonfiles/files/flash_dyson_v10.sh
+++ stage2/04-dysonfiles/files/flash_dyson_v10.sh
@@ -1,5 +1,34 @@
 #! /bin/bash -x
 {
+
+function unlock_mcu() {
+	echo "Bringing up SAMD20 in halt state"
+
+	raspi-gpio set 18 op dl
+	raspi-gpio set 25 op dl
+	sleep 1
+	raspi-gpio set 18 op dh
+
+	echo "Unlocking processor"
+
+	./openocd -f $CFG_FILE \
+             -c "transport select swd" \
+             -c "adapter speed 1000" \
+             -c "reset_config srst_only" \
+             -f at91samdXX.cfg  \
+             -c "init; at91samd.cpu mwb 0x41002100 0x10; reset; halt; exit"
+
+	return $?
+}
+
+function flash_mcu() {
+        ./openocd -f $CFG_FILE \
+                -c "transport select swd" \
+                -c "adapter speed 1000" \
+                -f at91samdXX.cfg \
+                -c "program $FILENAME verify reset exit"
+}
+
 echo "Preparing to reflash Dyson V10 battery pack"
 
 echo "Figuring out which kind of Pi we are running on"
@@ -30,33 +59,44 @@ else
 	exit 1
 fi
 
-echo "Bringing up SAMD20 in halt state"
-raspi-gpio set 18 op dl
-raspi-gpio set 25 op dl
-sleep 1
-raspi-gpio set 18 op dh
+RESULT=
+for i in `seq 10`; do
+	echo "Attempting to unlock/erase MCU - attempt ${i}"
+	unlock_mcu
+	RESULT=$?
+	if [ ${RESULT} -eq 0 ]; then
+		echo "Pack unlocked/erased successfully"
+		break
+	fi
+	echo "Unlock failed, pausing 2 seconds, then retrying"
+	sleep 2
+done
 
-echo "Unlocking processor"
+if [ ${RESULT} -ne 0 ]; then
+	echo "Unable to unlock pack - aborting"
+	exit 1
+fi
 
-./openocd -f $CFG_FILE \
-             -c "transport select swd" \
-             -c "adapter speed 1000" \
-	     -c "reset_config srst_only" \
-             -f at91samdXX.cfg  \
-             -c "init; at91samd.cpu mwb 0x41002100 0x10; halt; exit"
+RESULT=
+for i in `seq 10`; do
+	echo "Programming MCU - attempt ${i}"
+	flash_mcu
+	RESULT=$?
+	if [ ${RESULT} -eq 0 ]; then
+		echo "Programming successful"
+		break
+	fi
+	echo "Programming failed, pausing 2 seconds, then retrying"
+	sleep 2
+done
 
-echo "Reflashing"
-./openocd -f $CFG_FILE \
-	-c "transport select swd" \
-	-c "adapter speed 1000" \
-	-f at91samdXX.cfg \
-	-c "program $FILENAME verify reset exit"
+if [ ${RESULT} -ne 0 ]; then
+	echo "Unable to program pack - aborting"
+	exit 1
+fi
 
-[ $? -eq 0 ] && echo "Flashing completed successfully"
 
 } 2>&1 | tee -a /boot/flash_log.txt
 
+sync
 
-while true; do
-	sleep 1000
-done
